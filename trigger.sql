a8cda1197c02409c7abed69aef77dab19db9c5cf
ALISA <146487164+allfeia@users.noreply.github.com>
2024-05-01 16:32:37 +0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -2,23 +2,25 @@ CREATE OR REPLACE FUNCTION compare_planet_color()
 RETURNS TRIGGER AS $$
 BEGIN
     DECLARE
-        old_color TEXT;
-        new_color TEXT;
-    BEGIN
-        old_color := (SELECT planet_color FROM Observation WHERE id = OLD.id);
-        new_color := (SELECT planet_color FROM Observation WHERE id = NEW.id);
+        exist_color TEXT;
 
-        IF old_color <> new_color THEN
-            INSERT INTO Action (description, observation_id)
-            VALUES ('Цвет планеты изменился', NEW.id);
-        END IF;
-        
+    BEGIN
+        FOR exist_color IN
+            SELECT planet_color FROM Observation WHERE id < NEW.id
+        LOOP
+            IF exist_color <> NEW.planet_color THEN
+                INSERT INTO Action (description, observation_id)
+                VALUES ('Цвет планеты изменился', NEW.id);
+                EXIT;
+            END IF;
+        END LOOP;
+
         RETURN NEW;
     END;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER compare_color_trigger
-AFTER UPDATE OF planet_color ON Observation
+AFTER INSERT ON Observation
 FOR EACH ROW
 EXECUTE FUNCTION compare_planet_color();
